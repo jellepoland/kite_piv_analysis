16e8575eb5598065f1ba1b4f55b8c77e7cbec0eb
jellepoland <jellepoland@gmail.com>
2024-09-15 14:02:02 +0200

    trying to understand the stichting more

--- scripts/raw_to_processed_stitchted_data_edge_bug.py
+++ scripts/raw_to_processed_stitchted_data_edge_bug.py
@@ -13,14 +13,13 @@ import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap, Normalize
 from matplotlib.cm import ScalarMappable, get_cmap
 import matplotlib.ticker as mticker
+import pickle
 
 
-def read_dat_file(
+def read_single_dat_file(
     file_path: str,
     labbook_path: str,
     aoa_value: float,
-    x_meshgrid_global,
-    y_meshgrid_global,
     variables_edited,
 ):
     # Extract the case name and file name from the file path
@@ -167,6 +166,9 @@ def read_dat_file(
     else:
         variables_needing_flipping = []
 
+    # grab the global mesh grid
+    x_meshgrid_global, y_meshgrid_global = define_global_mesh_grid()
+
     sign = 1
     output = [case_name_davis]
     for k in range(2, len(variables_edited)):
@@ -216,61 +218,15 @@ def read_dat_file(
     return output
 
 
-# def save_meshgrid_data_csv(all_data, variable_names, save_path):
-#     # Create X and Y coordinates based on the shape of the data
-#     y, x = np.mgrid[0 : all_data[0].shape[0], 0 : all_data[0].shape[1]]
-
-#     # Create a dictionary to hold all the data
-#     data_dict = {"X": x.flatten(), "Y": y.flatten()}
-
-#     # Add each variable's data to the dictionary
-#     for data, var_name in zip(all_data, variable_names):
-#         data_dict[var_name] = data.flatten()
-
-#     # Create a DataFrame and save as CSV
-#     df = pd.DataFrame(data_dict)
-#     df.to_csv(save_path, index=False)
-
-#     print(f"Data saved to {save_path}")
-
-
-def process_all_dat_files(
-    variables_edited,
-    input_directory,
-    lab_book_path,
-    save_processed_folder,
-    save_plots_folder,
-    aoa_value,
-    vw,
-    plot_type=".png",
-    min_cbar_value=0.75,
-    max_cbar_value=1.25,
-):
-
-    logging.info(f"Processing all .dat files in directory: {input_directory}")
-
+def define_global_mesh_grid():
     # the x and y ranges are arbitrarly set, and resolution is calculated in the beginning
     x_global = np.arange(-210, 840, 2.4810164835164836)
     y_global = np.arange(-205, 405, 2.4810164835164836)
     x_meshgrid_global, y_meshgrid_global = np.meshgrid(x_global, y_global)
+    return x_meshgrid_global, y_meshgrid_global
 
-    output_list = []
-    for root, _, files in os.walk(input_directory):
-        for file in files:
-            # Only consider normal values, and not standard deviation
-            if file.endswith("1.dat"):
-                print(f"Processing file: {file}")
-                file_path = os.path.join(root, file)
-                output = read_dat_file(
-                    file_path,
-                    lab_book_path,
-                    aoa_value,
-                    x_meshgrid_global,
-                    y_meshgrid_global,
-                    variables_edited,
-                )
-                output_list.append(output)
 
+def get_y_grouped_filtered_data(output_list):
     # grouping the data on spanwise position (y-values)
     y1_data_list = ["Y1"]
     y2_data_list = ["Y2"]
@@ -310,168 +266,107 @@ def process_all_dat_files(
     for all_y_data in y_grouped_data:
         if len(all_y_data) > 1:
             y_grouped_filtered_data.append(all_y_data)
+    return y_grouped_filtered_data
+
+
+def process_planes(input_directory, lab_book_path, aoa_value, variables_edited):
+
+    # loop through all the files in the input_directory, and read the 6 planes
+    output_list = []
+    for root, _, files in os.walk(input_directory):
+        for file in files:
+            # Only consider normal values, and not standard deviation
+            if file.endswith("1.dat"):
+                print(f"Processing file: {file}")
+                file_path = os.path.join(root, file)
+                output = read_single_dat_file(
+                    file_path,
+                    lab_book_path,
+                    aoa_value,
+                    variables_edited,
+                )
+                output_list.append(output)
+    return output_list
+
+
+def stitch_planes(
+    y_grouped_filtered_data,
+    variables_edited,
+    save_processed_folder,
+):
 
     # averaging the data
     index_of_is_valid = variables_edited.index("is_valid") - 2
     print(f"index_of_is_valid:{index_of_is_valid}")
     for all_y_data in y_grouped_filtered_data:
-        logging.info(f"all_y_data len: {len(all_y_data)}")
-        y_num = all_y_data[0]
-        logging.info(f"y_num: {y_num}")
-        all_6_plane_data = all_y_data[1:]
-        logging.info(f"all_6_plane_data len: {len(all_6_plane_data)}")
 
+        y_num = all_y_data[0]
+        all_6_plane_data = all_y_data[1:]
         k_num = len(all_6_plane_data[0][1:])
         variables_processed = variables_edited[2 : 2 + k_num]
+        all_6_plane_data_mean_overlap_k_list = []
+
+        # logging
+        logging.info(f"all_y_data len: {len(all_y_data)}")
+        logging.info(f"y_num: {y_num}")
+        logging.info(f"all_6_plane_data len: {len(all_6_plane_data)}")
         logging.info(f"k_num: {k_num}")
         logging.info(f"variables_processed: {variables_processed}")
 
-        all_6_plane_data_mean_overlap_k_list = []
-        sum_bottom_k_list = []
+        # looping through each variable k (that was present in the .dat file)
         for k in range(k_num):
             sum_top = 0
             sum_bottom = 0
+            # loop through each plane of data
             for plane_idx, plane_data in enumerate(all_6_plane_data):
-                logging.info(f"plane_idx: {plane_idx}, k:{k}")
                 case_name = plane_data[0]
-                logging.info(f"case_name: {case_name}")
                 plane_data = plane_data[1:]
-                logging.info(f"plane_data shape: {np.array(plane_data).shape}")
                 data_k = plane_data[k]
-                logging.info(f"data_k shape: {data_k.shape}")
+
                 # Use the is_valid from .dat file to mask the data
-                # the astype int, ensure that it uses 0 and 1 as values
+                # The is_valid column tells you whether a particular data point is valid or not (likely represented by non-zero values).
+                # the astype int, converts the boolean mask (True/False) to an integer mask (1 for valid data, 0 for invalid data).
                 mask = (plane_data[index_of_is_valid] != 0).astype(int)
+
                 # Use the mask, to find only that data that is valid
+                #   For valid data points (where mask == 1), multiply the data_k values by the mask.
+                #   This ensures that only valid data contributes to the summation.
+                #   sum_top will be the sum of all VALID data points across all planes.
                 sum_top = sum_top + data_k * mask
+
                 # Use the mask to sum the number of valid data points
-                # each datapoint will thus be either 0,1,2,3,4 (max overlap)
+                #   The mask (which contains 1s for valid points and 0s for invalid points) is summed,
+                #   so sum_bottom will contain the total number of valid data points for each position
+                #   the number of valid data points will be either 0,1,2,3,4 (max overlap)
+                #   This tracks how many planes have valid data at each data point (used later for averaging).
                 sum_bottom = sum_bottom + mask
 
-                # new_data_k = data_k * mask
-                # # find where the data overlaps
-                # np.where(sum_bottom == 2, , 0)
+                # logging
+                logging.info(f"plane_idx: {plane_idx}, k:{k}")
+                logging.info(f"case_name: {case_name}")
+                logging.info(f"plane_data shape: {np.array(plane_data).shape}")
+                logging.info(f"data_k shape: {data_k.shape}")
 
             print(f"      --- variable: {variables_processed[k]} --- ")
-            # calculting mean, when not the is_valid column
+            # calculting mean, when is not the is_valid variable (or column)
             if k != index_of_is_valid:
                 print(f"-- handling stichting for k: {k} --")
                 # Avoid division by zero, by setting all zeros to 1
-                # this is not a problem as the sum top will be zero where sum_bottom is zero
-
-                # TODO: this is an alternative approach
-                # would allow you to smoothen things better
-                # plane_data_mean_overlap = np.zeros(sum_bottom.shape)
-                # for i in range(sum_bottom.shape[0]):
-                #     for j in range(sum_bottom.shape[1]):
-                #         if sum_bottom[i, j] == 0:
-                #             plane_data_mean_overlap[i, j] = np.nan
-                #         elif sum_bottom[i, j] == 1:
-                #             plane_data_mean_overlap[i, j] = sum_top[i, j]
-                #         elif sum_bottom[i, j] == 2:
-                #             plane_data_mean_overlap[i, j] = sum_top[i, j] / 2
-                #         elif sum_bottom[i, j] == 3:
-                #             plane_data_mean_overlap[i, j] = sum_top[i, j] / 3
-                #         elif sum_bottom[i, j] == 4:
-                #             plane_data_mean_overlap[i, j] = sum_top[i, j] / 4
-                #         else:
-                #             raise ValueError(
-                #                 f"to many datapoints here sum_bottom[i, j]: {sum_bottom[i, j]}"
-                #             )
-
-                # TODO: FAILED , to memory intensive attempt at radial basis function interpolation
-                # def radialbasisfunction_interpolation(sum_top, sum_bottom):
-                #     from scipy.interpolate import Rbf
-
-                #     # Determine the dimensions of the data
-                #     rows, cols = sum_bottom.shape
-
-                #     # Create grid coordinates
-                #     x, y = np.indices((rows, cols))
-
-                #     # Flatten the arrays and select valid points
-                #     x_flat = x.flatten()
-                #     y_flat = y.flatten()
-                #     sum_top_flat = sum_top.flatten()
-                #     sum_bottom_flat = sum_bottom.flatten()
-
-                #     # Identify valid points where sum_bottom is not zero
-                #     valid_indices = sum_bottom_flat > 0
-                #     x_valid = x_flat[valid_indices]
-                #     y_valid = y_flat[valid_indices]
-                #     z_valid = (
-                #         sum_top_flat[valid_indices] / sum_bottom_flat[valid_indices]
-                #     )
-
-                #     # Create the RBF interpolator
-                #     rbf = Rbf(x_valid, y_valid, z_valid, function="linear")
-
-                #     # Generate the grid of coordinates for interpolation
-                #     xi, yi = np.indices((rows, cols))
-
-                #     # Interpolate the data over the full grid
-                #     plane_data_mean_overlap = rbf(xi, yi)
-
-                #     # plane_data_mean_overlap now contains the interpolated values
-
-                #     # To handle the areas where sum_bottom was zero, you may want to keep these as NaN or some default value.
-                #     plane_data_mean_overlap[sum_bottom == 0] = np.nan
-
-                #     return plane_data_mean_overlap
-
-                # plane_data_mean_overlap = radialbasisfunction_interpolation(
-                #     sum_top, sum_bottom
-                # )
-
-                # TODO: attempt at nearest neighbour interpolation
-                # def nearestneighbour_interpolation(sum_bottom, sum_top):
-                #     # Determine the dimensions of the data
-                #     rows, cols = sum_bottom.shape
-
-                #     # Create grid coordinates
-                #     x, y = np.indices((rows, cols))
-
-                #     # Flatten the arrays and select valid points
-                #     x_flat = x.flatten()
-                #     y_flat = y.flatten()
-                #     sum_top_flat = sum_top.flatten()
-                #     sum_bottom_flat = sum_bottom.flatten()
-
-                #     # Identify valid points where sum_bottom is not zero
-                #     valid_indices = sum_bottom_flat > 0
-                #     points = np.column_stack(
-                #         (x_flat[valid_indices], y_flat[valid_indices])
-                #     )
-                #     values = (
-                #         sum_top_flat[valid_indices] / sum_bottom_flat[valid_indices]
-                #     )
-
-                #     # Interpolation grid
-                #     xi, yi = np.indices((rows, cols))
-
-                #     # Perform Nearest Neighbour interpolation
-                #     plane_data_mean_overlap = griddata(
-                #         points, values, (xi, yi), method="nearest"
-                #     )
-
-                #     # Set areas where there was no valid data to NaN
-                #     plane_data_mean_overlap[sum_bottom == 0] = np.nan
-                #     return plane_data_mean_overlap
-
-                # plane_data_mean_overlap = nearestneighbour_interpolation(
-                #     sum_bottom, sum_top
-                # )
-
-                # print(f"shape bottom: {sum_bottom.shape}, shape top: {sum_top.shape}")
+                #   this is not a problem as the sum top will be zero where sum_bottom is zero
+                #   Replacing zeros in sum_bottom with nan values
                 sum_bottom = np.where(sum_bottom == 0, np.nan, sum_bottom)
+
+                # Calculate the mean overlap / average value
+                #   valid data divided by the number of valid data points
                 plane_data_mean_overlap = sum_top / sum_bottom
+
             # if is_valid column, don't take mean, simply take the is_valid counter
             else:
                 plane_data_mean_overlap = sum_bottom
             logging.info(
                 f"plane_data_mean_overlap: {plane_data_mean_overlap.shape}, max: {np.max(plane_data_mean_overlap)}, min: {np.min(plane_data_mean_overlap)}"
             )
-            sum_bottom_k_list.append(sum_bottom)
+
             all_6_plane_data_mean_overlap_k_list.append(plane_data_mean_overlap)
 
         logging.info(
@@ -479,6 +374,8 @@ def process_all_dat_files(
         )
 
         # Saving the data
+        x_meshgrid_global, y_meshgrid_global = define_global_mesh_grid()
+
         save_path = save_processed_folder + f"{y_num}.csv"
         data_dict = {"x": x_meshgrid_global.flatten(), "y": y_meshgrid_global.flatten()}
         # Add each MEANED variable's data to the dictionary
@@ -490,6 +387,8 @@ def process_all_dat_files(
         # Create a DataFrame and save as CSV
         df = pd.DataFrame(data_dict)
         df.to_csv(save_path, index=False)
+
+    return df
 
 
 if __name__ == "__main__":
@@ -543,60 +442,67 @@ if __name__ == "__main__":
         "is_valid",
     ]
 
+    input_directory = sys.path[0] + "/data/y2_testing_for_edge_bug/"
     lab_book_path = sys.path[0] + "/data/labbook_cleaned.csv"
-    save_processed_folder = sys.path[0] + "/processed_data/"
+    save_processed_folder = sys.path[0] + "/processed_data/y2_testing_for_edge_bug/"
     save_plots_folder = sys.path[0] + "/results/"
+    aoa_value = 13
 
-    # Process all .dat files
-    process_all_dat_files(
+    # # Process the data
+    # output_list = process_planes(
+    #     input_directory, lab_book_path, aoa_value, variables_edited
+    # )
+    # # Group the data on y-values
+    # y_grouped_filtered_data = get_y_grouped_filtered_data(output_list)
+
+    # # save the data using pickle
+    # with open(save_processed_folder + "y_grouped_filtered_data.pkl", "wb") as f:
+    #     pickle.dump(y_grouped_filtered_data, f)
+
+    # load the data using pickle
+    with open(save_processed_folder + "y_grouped_filtered_data.pkl", "rb") as f:
+        y_grouped_filtered_data = pickle.load(f)
+
+    # Stich the data together
+    df_stichted = stitch_planes(
+        y_grouped_filtered_data,
         variables_edited,
-        input_directory=sys.path[0] + "/data/test_y2/",
-        lab_book_path=sys.path[0] + "/data/labbook_cleaned.csv",
-        save_processed_folder=sys.path[0] + "/processed_data/y1_testing_for_edge_bug/",
-        # save_processed_folder=sys.path[0] + "/processed_data/",
-        save_plots_folder=sys.path[0] + "/results/aoa_13/all_planes/",
-        aoa_value=13.0,
-        vw=15.0,
-        min_cbar_value=5,
-        max_cbar_value=20,
-        plot_type=".png",
+        save_processed_folder,
+    )
+    # # # Load the processed data
+    # loaded_data = pd.read_csv(save_processed_folder + f"Y2.csv")
+    # # loaded_data = pd.read_csv(save_processed_folder + f"Y2.csv")
+    # logging.info(f"loaded_data:{loaded_data}")
+    loaded_data = df_stichted
+
+    x_meshgrid_global, y_meshgrid_global = define_global_mesh_grid()
+
+    vel_u_on_meshgrid_global = griddata(
+        np.array([x_meshgrid_global.flatten(), y_meshgrid_global.flatten()]).T,
+        np.array(loaded_data["vel_u"].values),
+        (x_meshgrid_global, y_meshgrid_global),
+        method="linear",
+    )
+    vel_v_on_meshgrid_global = griddata(
+        np.array([x_meshgrid_global.flatten(), y_meshgrid_global.flatten()]).T,
+        np.array(loaded_data["vel_v"].values),
+        (x_meshgrid_global, y_meshgrid_global),
+        method="linear",
     )
 
-    # # Load the processed data
-    # loaded_data = pd.read_csv(
-    #     save_processed_folder + f"/y1_testing_for_edge_bug/Y1.csv"
-    # )
-    # logging.info(f"loaded_data:{loaded_data}")
-    # x_global = np.arange(-210, 840, 2.4810164835164836)
-    # y_global = np.arange(-205, 405, 2.4810164835164836)
-    # x_meshgrid_global, y_meshgrid_global = np.meshgrid(x_global, y_global)
+    from plotting_single_variable import saving_a_plot
 
-    # vel_u_on_meshgrid_global = griddata(
-    #     np.array([x_meshgrid_global.flatten(), y_meshgrid_global.flatten()]).T,
-    #     np.array(loaded_data["vel_u"].values),
-    #     (x_meshgrid_global, y_meshgrid_global),
-    #     method="linear",
-    # )
-    # vel_v_on_meshgrid_global = griddata(
-    #     np.array([x_meshgrid_global.flatten(), y_meshgrid_global.flatten()]).T,
-    #     np.array(loaded_data["vel_v"].values),
-    #     (x_meshgrid_global, y_meshgrid_global),
-    #     method="linear",
-    # )
-
-    # from plotting_single_variable import saving_a_plot
-
-    # saving_a_plot(
-    #     x_meshgrid_global=x_meshgrid_global,
-    #     y_meshgrid_global=y_meshgrid_global,
-    #     u_for_quiver=vel_u_on_meshgrid_global,
-    #     v_for_quiver=vel_v_on_meshgrid_global,
-    #     color_data=vel_u_on_meshgrid_global / 15.0,
-    #     save_plots_folder=save_plots_folder,
-    #     plot_type=".pdf",
-    #     min_cbar_value=0.75,
-    #     max_cbar_value=1.25,
-    #     max_mask_value=1.75,
-    #     min_mask_value=0.75,
-    #     title="y1_testing_for_edge_bug",
-    # )
+    saving_a_plot(
+        x_meshgrid_global=x_meshgrid_global,
+        y_meshgrid_global=y_meshgrid_global,
+        u_for_quiver=vel_u_on_meshgrid_global,
+        v_for_quiver=vel_v_on_meshgrid_global,
+        color_data=vel_u_on_meshgrid_global / 15.0,
+        save_plots_folder=save_plots_folder,
+        plot_type=".pdf",
+        min_cbar_value=0.75,
+        max_cbar_value=1.25,
+        max_mask_value=1.75,
+        min_mask_value=0.75,
+        title="y2_testing_for_edge_bug",
+    )
